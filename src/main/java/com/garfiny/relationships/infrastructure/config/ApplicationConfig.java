ab2c7493b97684ce58242deb2f9cd39673a373a2
shuoz <shuoz@email.com>
2012-07-16 16:41:44 +1000

    java based configuration web app step 1 -- hellow world works

--- src/main/java/com/garfiny/relationships/infrastructure/config/ApplicationConfig.java
+++ src/main/java/com/garfiny/relationships/infrastructure/config/ApplicationConfig.java
@@ -1,11 +1,15 @@
 package com.garfiny.relationships.infrastructure.config;
 
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.ImportResource;
 
 @Configuration
+@ComponentScan("com.garfiny.relationships")
 @ImportResource("classpath:spring/root-context.xml")
+
+@Import(MvcConfig.class)
 public class ApplicationConfig {
 
-	
 }
