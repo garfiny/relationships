84e5d957a801687d84043c1095055b46b120102e
shuoz <shuoz@email.com>
2012-07-27 16:59:46 +1000

    Added spring security, but it's not working yet

--- src/main/java/com/garfiny/relationships/infrastructure/config/SpringWebApplicationInitializer.java
+++ src/main/java/com/garfiny/relationships/infrastructure/config/SpringWebApplicationInitializer.java
@@ -1,5 +1,6 @@
 package com.garfiny.relationships.infrastructure.config;
 
+import javax.servlet.FilterRegistration.Dynamic;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
@@ -9,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.DelegatingFilterProxy;
 import org.springframework.web.servlet.DispatcherServlet;
 
 public class SpringWebApplicationInitializer implements WebApplicationInitializer {
@@ -28,6 +30,9 @@ public class SpringWebApplicationInitializer implements WebApplicationInitialize
 		
 		// Manage the lifecycle of the root application context
 		container.addListener(new ContextLoaderListener(rootContext));
+		
+		// make the application use spring security framework
+		addSecurityAbility(container);
 
 		// Allows attributes to be accessed on the next request
 		// TODO figure out what's FlashMapFilter
@@ -52,6 +57,11 @@ public class SpringWebApplicationInitializer implements WebApplicationInitialize
 //	    h2Servlet.setInitParameter("webAllowOthers", "true");
 //	    h2Servlet.setLoadOnStartup(2);
 //	    h2Servlet.addMapping("/admin/h2/*");
+	}
+	
+	private void addSecurityAbility(ServletContext container) {
+		Dynamic filter = container.addFilter("springSecurityFilterChain", DelegatingFilterProxy.class);
+		filter.addMappingForUrlPatterns(null, false, "/*");
 	}
 
 }
